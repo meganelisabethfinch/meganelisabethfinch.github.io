086fb99f1b6ed44f49d3c26bcda64ee4f7be1768
Megan Elisabeth Finch <meganelisabethfinch@gmail.com>
2020-11-01 20:39:18 +0000

    Hardcoded embed of Cosmic Taxi

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,13 +6,21 @@ import { articles } from './data';
 // if (article == undefined), render 404
 // else render the article
 
-const Article = ({match:{params:{id}}}) => (
-    <Col lg={true} className="article">
-        <h2 className="custom-brand">{articles.find(x => x.id == id).title}</h2>
+function Article({match:{params:{id}}}) {
+    const article = articles.find(x => x.id == id);
+    if (article == undefined) {
+        article = articles.find(x => x.id == "404");
+    } 
+    return <Col lg={true} className="article">
+        <h2 className="custom-brand">{article.title}</h2>
+        <iframe frameborder="0" src="https://itch.io/embed-upload/2916835?color=ffffff" allowfullscreen="" width="1100" height="750">
+            <a href="https://imaginaryaround.itch.io/cosmic-taxi">Play Cosmic Taxi on itch.io</a>
+        </iframe>
         <p>
             hi there
         </p>
     </Col>
-);
+    
+};
 
 export default Article;
