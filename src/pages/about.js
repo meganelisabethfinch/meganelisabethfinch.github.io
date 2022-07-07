9469b35b687a160e30a05aafe89084781cd75333
meganelisabethfinch <meganelisabethfinch@gmail.com>
2022-07-07 22:40:26 +0100

    updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,15 +3,18 @@ import { Col } from 'react-bootstrap';
 
 const About = () => (
     <Col lg={true} className="article">
-            <h2 className="custom-brand">About Me</h2>
+            <h2 className="my-brand">About Me</h2>
             <p>
-                Hi, my name is Megan. I'm a second-year computer science student at the University of Cambridge.
+                Hi, my name is Megan. I am an MS student at the Music and Audio Computing Lab (MACLab) at KAIST.
+            </p>
+            <p>
+                My main interests are in computer music, computer vision and digital signal processing.
             </p>
             <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sem diam, tincidunt et augue et, condimentum semper nisi. Phasellus euismod ex sit amet lacus consectetur euismod nec non sem. Nullam porttitor mi a cursus tempus. Integer facilisis dapibus urna vitae sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
             </p>
             <p>
-                I made this blog using React to showcase my projects. Watch this space!
+                I made this blog using React to showcase my projects.
             </p>
             <p>
                 <h4>Contact</h4>
