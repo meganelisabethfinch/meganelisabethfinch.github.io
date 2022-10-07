a6f9e3d0a41f58f6a4d54e452c71c2f2a96b83b9
meganelisabethfinch <meganelisabethfinch@gmail.com>
2022-10-07 22:00:37 +0900

    Updates

---
+++ precache-manifest.e4594838d2372be6f21f10fbb59b8fea.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bac8283a39f53bde191972d4bae4583",
+    "url": "/meganelisabethfinch.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "7d58d3782a83d1fd8a53",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/css/3.168a2b2b.chunk.css"
+  },
+  {
+    "revision": "fdd1eaa0d3ebb995b5a8",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/css/4.f913313e.chunk.css"
+  },
+  {
+    "revision": "bec5b6ca0e496ebe19fa",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/css/5.df92ce43.chunk.css"
+  },
+  {
+    "revision": "6107bed68b25a199096d",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/css/6.df92ce43.chunk.css"
+  },
+  {
+    "revision": "524166d6f47f878efec1",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/css/7.f7b12d47.chunk.css"
+  },
+  {
+    "revision": "109f9e61e28f4a4ec00d",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/css/main.88474af9.chunk.css"
+  },
+  {
+    "revision": "ff5eda0dc0f555cb3d49",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/js/0.da04e58b.chunk.js"
+  },
+  {
+    "revision": "3df54bba2137ec524f3fb39f2c61461a",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/js/0.da04e58b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d58d3782a83d1fd8a53",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/js/3.ed77d039.chunk.js"
+  },
+  {
+    "revision": "42780b3dfc4008075422892f0026a2f6",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/js/3.ed77d039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd1eaa0d3ebb995b5a8",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/js/4.bb0d1f68.chunk.js"
+  },
+  {
+    "revision": "bec5b6ca0e496ebe19fa",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/js/5.981430b5.chunk.js"
+  },
+  {
+    "revision": "6107bed68b25a199096d",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/js/6.6bb79920.chunk.js"
+  },
+  {
+    "revision": "524166d6f47f878efec1",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/js/7.95de046c.chunk.js"
+  },
+  {
+    "revision": "b48b3719851c7c9fe92a7dc1172da0e1",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/js/7.95de046c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "109f9e61e28f4a4ec00d",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/js/main.a71b290e.chunk.js"
+  },
+  {
+    "revision": "e8b5afba4172dec8524a",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/js/runtime-main.0adc88b7.js"
+  },
+  {
+    "revision": "1a38e5824a9fc1e042e64c336d926bee",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/complex-pipeline.1a38e582.png"
+  },
+  {
+    "revision": "719226103d61efcc0a04118f5257c762",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/cosmic-taxi.71922610.jpg"
+  },
+  {
+    "revision": "a4c5735c8752b5412d7a13bbcf6aac89",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/cosmic-taxi.a4c5735c.png"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/genetic-music.d41d8cd9.md"
+  },
+  {
+    "revision": "a9b7c62a96aa7a5a391f8d068a318b91",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/graduation.a9b7c62a.png"
+  },
+  {
+    "revision": "90d5e709b3c411a8d9401676555d9307",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/intro.90d5e709.md"
+  },
+  {
+    "revision": "e5ca2fc3fdc6fdcd7499393622454ab0",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/midpoint.e5ca2fc3.png"
+  },
+  {
+    "revision": "ca8556a5117a56754e22d340f3c5891d",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/newnham-matches.ca8556a5.png"
+  },
+  {
+    "revision": "9353602514692f490460a4891248bcb6",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/ray-tracer.93536025.png"
+  },
+  {
+    "revision": "e0b4c9f03e386bf40a52747ad54e0619",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/ray-tracer.e0b4c9f0.md"
+  },
+  {
+    "revision": "bf6c0829756253c1a8d2517728bdde75",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/sfm-thumb.bf6c0829.png"
+  },
+  {
+    "revision": "395a4a7d82dbfd058d895b05c23b8542",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/simple-pipeline.395a4a7d.png"
+  },
+  {
+    "revision": "906f28d61dbf516801d7c13f2fecd250",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/structure-from-motion-thumb.906f28d6.png"
+  },
+  {
+    "revision": "3eafce2b18b8819e8ab2fba5a0bab980",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/structure-from-motion.3eafce2b.md"
+  },
+  {
+    "revision": "9353602514692f490460a4891248bcb6",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/test.93536025.png"
+  },
+  {
+    "revision": "ee31bacc0f026798d581672f46add98a",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/triangulation.ee31bacc.png"
+  },
+  {
+    "revision": "1dc0513a57588df184037975731906b6",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/triolingo.1dc0513a.png"
+  },
+  {
+    "revision": "c7384a4367daa56ef3c72229295b2199",
+    "url": "/meganelisabethfinch.github.io/portfolio/static/media/triolingo.c7384a43.md"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f4d359cc5f5d5fb5065f839e9c83502.js"
+  "/meganelisabethfinch.github.io/portfolio/precache-manifest.e4594838d2372be6f21f10fbb59b8fea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/meganelisabethfinch.github.io/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
